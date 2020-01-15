b576a28368f7c7fb8739cf2dab9f643ff1a3c35e
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2020-01-15 15:39:14 -0600

    add docker hub
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>

---
+++ ci/ci-dockerhub.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+# Get the conformance.test binary
+if [[ ! -f testbin/conformance.test ]]; then
+  rm -rf distribution-spec/
+  git clone https://github.com/bloodorangeio/distribution-spec.git
+  pushd distribution-spec/conformance/
+  go test -c
+  popd
+  mkdir -p testbin/
+  mv distribution-spec/conformance/conformance.test testbin/conformance.test
+  rm -rf distribution-spec/
+fi
+
+export OCI_ROOT_URL="https://registry-1.docker.io"
+export OCI_NAMESPACE="${OCI_NAMESPACE:-bloodorangeio/oci-conformance-test}"
+export OCI_USERNAME="${DOCKERHUB_USERNAME}"
+export OCI_PASSWORD="${DOCKERHUB_PASSWORD}"
+
+set +e
+testbin/conformance.test
+RC="$?"
+set -e
+
+mkdir -p testresults/
+if [[ -f junit.xml ]]; then
+  mv junit.xml testresults/junit-dockerhub.xml
+fi
+if [[ -f report.html ]]; then
+  mv report.html testresults/report-dockerhub.html
+fi
+
+exit ${RC}
